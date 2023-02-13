e9592ca021fb93221a47371b2cd3bdd7ee8046d1
Francisco Berti da Cruz <fbc991@live.com>
2023-02-13 10:25:05 -0300

    setup do projeto

---
+++ src/mains.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(3, 2));
+function saudacao(a) {
+    return `Olá ${a}`;
+}
+console.log(saudacao('francisco'));
---
+++ src/mains.ts
@@ -0,0 +1,13 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+
+}
+
+console.log(multiplicacao(3, 2))
+
+
+function saudacao(a: string): string {
+    return `Olá ${a}`
+}
+
+console.log(saudacao('francisco'))
